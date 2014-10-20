362b834282c9b24b838507df8599494c7c1b511b
weefbellington <weefbellington@gmail.com>
2014-10-20 11:07:21 -0400

    add (optional) overlay to modal director that blocks background touches

--- library/src/main/java/com/davidstemmer/screenplay/scene/director/ModalDirector.java
+++ library/src/main/java/com/davidstemmer/screenplay/scene/director/ModalDirector.java
@@ -1,8 +1,10 @@
 package com.davidstemmer.screenplay.scene.director;
 
 import android.content.Context;
+import android.view.View;
 import android.view.ViewGroup;
 
+import com.davidstemmer.screenplay.R;
 import com.davidstemmer.screenplay.SceneCut;
 import com.davidstemmer.screenplay.scene.Scene;
 
@@ -15,21 +17,48 @@ import flow.Flow;
  */
 public class ModalDirector implements Scene.Director {
 
+    private boolean showsBackgroundOverlay = true;
+
     @Inject
     public ModalDirector() {}
 
     @Override
     public void layoutNext(Context context, ViewGroup parent, SceneCut cut) {
         if (cut.direction == Flow.Direction.FORWARD || cut.direction == Flow.Direction.REPLACE) {
+            if (showsBackgroundOverlay) {
+                parent.addView(createBackgroundOverlay(parent));
+            }
             parent.addView(cut.nextScene.setUp(context, parent));
         }
     }
 
-
     @Override
     public void layoutPrevious(Context context, ViewGroup parent, SceneCut cut) {
         if (cut.direction == Flow.Direction.BACKWARD) {
+            if (showsBackgroundOverlay) {
+                parent.removeView(parent.findViewById(getOverlayId()));
+            }
             parent.removeView(cut.previousScene.tearDown(context, parent));
         }
+    }
+
+    public void showsBackgroundOverlay(boolean show) {
+        this.showsBackgroundOverlay = show;
+    }
+
+    protected View createBackgroundOverlay(ViewGroup parent) {
+        View overlay = new View(parent.getContext());
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT);
+        overlay.setLayoutParams(params);
+        overlay.setBackgroundColor(0xA0000000);
+        overlay.setId(getOverlayId());
+        overlay.setClickable(true);
+        return overlay;
+    }
+
+    protected int getOverlayId() {
+        return R.id.modal_overlay;
     }
 }
