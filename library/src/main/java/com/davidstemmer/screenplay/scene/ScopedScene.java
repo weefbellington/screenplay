893862f940f409acf108e6678038d95bc1efa03b
weefbellington <weefbellington@gmail.com>
2015-02-26 18:12:57 -0500

    Modify the ScopedScene so that it creates the scope in the constructor and destroys it in tearDown, if the direction is BACK. This allows you to inject dependencies in the scene without relying on a proxy object. Also have it implement the `Scoped` interface.

--- library/src/main/java/com/davidstemmer/screenplay/scene/ScopedScene.java
+++ library/src/main/java/com/davidstemmer/screenplay/scene/ScopedScene.java
@@ -4,49 +4,75 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Arrays;
+import java.util.List;
+
 import flow.Flow;
 import mortar.Blueprint;
 import mortar.Mortar;
 import mortar.MortarScope;
+import mortar.Scoped;
 
 /**
  * Created by weefbellington on 10/20/14.
  */
-public abstract class ScopedScene extends StandardScene implements Blueprint {
+public abstract class ScopedScene extends StandardScene implements Scoped, Blueprint {
 
-    private MortarScope scope;
+    private final MortarScope scope;
+    private final Context scopedContext;
+    private final List modules;
+
+    protected ScopedScene(Context context, Object...modules) {
+        this.modules = Arrays.asList(modules);
+        this.scope = createScope(context);
+        this.scopedContext = createMortarContext(context);
+    }
 
     @Override
     public View setUp(Context context, ViewGroup parent, Flow.Direction direction) {
-        return super.setUp(createScope(context), parent, direction);
+        return super.setUp(scopedContext, parent, direction);
     }
 
     @Override
     public View tearDown(Context context, ViewGroup parent, Flow.Direction direction) {
         View destroyed = super.tearDown(context, parent, direction);
-        destroyScope(context);
+        if (direction != Flow.Direction.FORWARD) {
+            destroyScope(context);
+        }
         return destroyed;
     }
 
-
-    private Context createScope(Context context) {
+    private MortarScope createScope(Context context) {
         MortarScope parentScope = Mortar.getScope(context);
-        scope = parentScope.requireChild(this);
-        Context childContext = scope.createContext(context);
+        return parentScope.requireChild(this);
+    }
 
+    private Context createMortarContext(Context context) {
+        Context childContext = scope.createContext(context);
         Mortar.inject(childContext, this);
-        onCreateScope(scope);
+        scope.register(this);
         return childContext;
     }
 
     private void destroyScope(Context context) {
         MortarScope parentScope = Mortar.getScope(context);
         parentScope.destroyChild(scope);
-        scope = null;
-        onDestroyScope();
     }
 
-    public void onCreateScope(MortarScope scope) {}
 
-    public void onDestroyScope() {}
+    @Override
+    public void onExitScope() {}
+
+    @Override
+    public void onEnterScope(MortarScope scope) {}
+
+    @Override
+    public String getMortarScopeName() {
+        return getClass().getName();
+    }
+
+    @Override
+    public Object getDaggerModule() {
+        return modules;
+    }
 }
