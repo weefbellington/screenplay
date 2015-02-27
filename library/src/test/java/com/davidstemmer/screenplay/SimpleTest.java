3a7472f9afba0e877b5c87e8903b7a7653b13081
weefbellington <weefbellington@gmail.com>
2015-02-27 01:29:20 -0500

    use the robolectric plugin with newest version of gradle build tools

--- library/src/test/java/com/davidstemmer/screenplay/SimpleTest.java
+++ library/src/test/java/com/davidstemmer/screenplay/SimpleTest.java
@@ -9,11 +9,10 @@ import org.robolectric.annotation.Config;
  * Created by weefbellington on 1/31/15.
  */
 @RunWith(RobolectricTestRunner.class)
-@Config(emulateSdk=18, manifest="./src/test/AndroidManifest.xml")
 public class SimpleTest {
 
     @Test
     public void shouldFail() {
-        assert(true);
+        assert(false);
     }
 }
